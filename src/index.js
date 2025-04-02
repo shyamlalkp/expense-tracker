35ad6a9ef6dde1886927af7a610f62576b91f8f0
shyamlalkp <146191497+shyamlalkp@users.noreply.github.com>
2025-04-02 19:36:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,10 +19,3 @@ root.render(
   </React.StrictMode>
 );
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { createRoot } from "react-dom/client";
-// import App from "./App";
-
-// const root = createRoot(document.getElementById("root"));
-// root.render(<App />);
