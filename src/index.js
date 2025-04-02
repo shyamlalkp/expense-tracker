9ad05a0eb4e91d124eafb981bf51b0f7ca5878de
shyamlalkp <146191497+shyamlalkp@users.noreply.github.com>
2025-04-02 19:36:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalProvider } from "./context/GlobalState";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <GlobalProvider>
-//       <App />
-//     </GlobalProvider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
