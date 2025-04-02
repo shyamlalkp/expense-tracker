47e0ad6d6ec65cf1105ad571c0d7d6929c4b76f0
shyamlalkp <146191497+shyamlalkp@users.noreply.github.com>
2025-04-02 23:58:42 +0530

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,7 +4,6 @@ import { TransactionComponent } from "./TransactionComponent";
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
-  // console.log(transactions);
   return (
     <>
       <h3>History</h3>
