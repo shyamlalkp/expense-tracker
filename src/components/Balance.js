ed508f9b0cf82e7590a6abcf45f7b146562802f2
shyamlalkp <146191497+shyamlalkp@users.noreply.github.com>
2025-04-07 22:02:30 +0530

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,7 +3,7 @@ import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
-  const amounts = transactions.map((data) => data.amount);
+  const amounts = transactions.map((data) => data.amount); //map creates a new array from transactions.
   console.log(amounts);
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
